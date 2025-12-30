0800dd8bdd81282d2891c3b538b54059c4f31a90
Garima-Gayakwad <garimag.2006@gmail.com>
2025-12-30 11:00:00 +0530

    feat: add landing page entry point

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
+import { Problem } from "@/components/Problem";
+import { Features } from "@/components/Features";
+import { HowItWorks } from "@/components/HowItWorks";
+import { Impact } from "@/components/Impact";
+import { Demo } from "@/components/Demo";
+import { Vision } from "@/components/Vision";
+import { FinalCTA } from "@/components/FinalCTA";
+import { Footer } from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <main>
+        <Hero />
+        <Problem />
+        <Features />
+        <HowItWorks />
+        <Impact />
+        <Demo />
+        <Vision />
+        <FinalCTA />
+        <Footer />
+      </main>
+    </div>
+  );
+}
